92002c2f45cde8069e1c751468081fa630d50a66
Wenli Xu <wikibingo@aim.com>
2023-10-14 13:41:54 -0700

    git initialized

---
+++ main.js
@@ -0,0 +1,18 @@
+const path = require("path");
+/*
+ * Project: Milestone 1
+ * File Name: main.js
+ * Description:
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const IOhandler = require("./IOhandler");
+const zipFilePath = path.join(__dirname, "myfile.zip");
+const pathUnzipped = path.join(__dirname, "unzipped");
+const pathProcessed = path.join(__dirname, "grayscaled");
+
+
+
